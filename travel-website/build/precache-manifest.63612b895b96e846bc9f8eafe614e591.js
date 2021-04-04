f6d29b23ca582fbc304619da637e96846cba7b5a
Altan Yeşilkurt <altan.yesilkurt@litum.com>
2021-04-04 23:12:10 +0300

    build commit

---
+++ travel-website/build/precache-manifest.63612b895b96e846bc9f8eafe614e591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0082fa19dbb0b9f2c33625e3d19a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "128e37c37366d7b409c7",
+    "url": "/static/css/main.1b686a40.chunk.css"
+  },
+  {
+    "revision": "4d2255290b1643f4db03",
+    "url": "/static/js/2.4c088ce1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4c088ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128e37c37366d7b409c7",
+    "url": "/static/js/main.1ed982d6.chunk.js"
+  },
+  {
+    "revision": "7a1bc4d8c7920d0dc650",
+    "url": "/static/js/runtime-main.53613ccf.js"
+  }
+]);
